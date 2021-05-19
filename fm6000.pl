17e3167c1948b1c1a111eab860d7025d357a5dc8
anhsirk0 <krishna404@yandex.com>
2021-05-20 01:03:28 +0530

    Changed pacs for debian based

--- fm6000.pl
+++ fm6000.pl
@@ -91,7 +91,7 @@ sub packages {
     # for arch based
     my $pacs = `pacman -Q 2>/dev/null`;
     # for debian based
-    unless ($pacs) { $pacs = `dpkg-query -f '\n' -W 2>/dev/null` }
+    unless ($pacs) { $pacs = `dpkg-query -l | grep "^ii"` }
     # for fedora
     unless ($pacs) { $pacs = `yum list installed 2>/dev/null` }
     # for BSD
