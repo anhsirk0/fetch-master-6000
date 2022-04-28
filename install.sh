0f25b13605bad44a66e1113c6801fc7c8c94984d
XDream8 <ism.frat@hotmail.com>
2022-04-28 20:43:56 +0300

    posix compatible and colored install_script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 
 
@@ -6,22 +6,29 @@
 [ -z "$root" ] && root=0
 
 ## colors
+### use RED color for unsuccessful operations or else
 RED='\033[0;31m'
+### use GREEN color for successful operations
+GREEN='\033[1;32m'
+### file related operations
 YELLOW='\033[1;33m'
+### use BLUE color for script operations
 BLUE='\033[0;34m'
+### color when installing fm6000
 CYAN='\033[0;36m'
+### clear colors
 NC='\033[0m'
 
 if [ -f "fm6000.pl" ]; then
-    echo "Seems like you cloned the repository"
+    printf '%b\n' "${BLUE}Seems like you cloned the repository${NC}"
     cp fm6000.pl fm6000
 else
     url="https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl"
     if [[ $(command -v curl) ]]; then
-        echo "Downloading the script"
+        printf '%b\n' "${BLUE}Downloading the script${NC}"
         curl $url -o fm6000
     else
-        echo "curl is required" && exit
+        printf '%b\n' "${RED}curl is required${NC}" && exit 1
     fi
 fi
 
@@ -32,30 +39,33 @@ else
 fi
 
 if [ -f "fm6000" ] && [ -s "fm6000" ]; then
-    chmod +x fm6000
-    echo "Making the script executable : done"
+    chmod +x fm6000 && printf '%b\n' "${BLUE}Making the script executable : ${GREEN}done"
+		require_text="root required"
     if [ -x $install_path ]; then
 				if [ $root = 0 ]; then
 					require_text="root not required"
 					sudo=
-				else
-					require_text="root required"
 				fi
+				printf '%b' "${YELLOW}"
         read -p "Move the script to $install_path [$require_text]? (y/N) " ans
     else
         install_path=/usr/local/bin
-        read -p "Move the script to $install_path [root required]? (y/N) " ans
+				printf '%b' "${YELLOW}"
+        read -p "Move the script to $install_path [$require_text]? (y/N)  " ans
     fi
 
     if [ "${ans}" = "y" ]; then
-        echo "Moving fm6000 to $install_path"
-        $sudo mv -v fm6000 $install_path || printf '%s\n' "error: $sudo failed" && exit 1
+        printf '%b\n' "${BLUE}Moving fm6000 to $install_path${NC}"
+				printf '%b' "${CYAN}"
+				$sudo mv -v fm6000 $install_path || ( printf '%b\n' "${RED}error: $sudo failed${NC}"; exit; exit 1 ) # double exit. first one exits the function
+				printf '%b\n' "${NC}"
+				printf '%b\n' "${GREEN}Fetch-master-6000 is successfully installed${NC}"
+				exit 0
     else
-        echo "Script not moved"
+        printf '%b\n' "${YELLOW}Script not moved!${NC}"
         ./fm6000
-        exit
+        exit 0
     fi
-    echo "Fetch-master-6000 is succesfully installed"
 else
-    echo "Unable to download the script"
+    printf '%b\n' "${RED}Unable to download the script${NC}"
 fi
